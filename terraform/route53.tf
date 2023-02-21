116c270183a8e7f93d176eb8a7928c21a3d6f933
Tim Myers <timmyers09@gmail.com>
2023-02-21 11:53:28 -0700

    infra: add dns records

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -7,15 +7,27 @@ module "route53_zone_din_dev" {
   environment = var.environment
 }
 
-# resource "aws_route53_record" "runner_custom_domain_record" {
-#   count = var.environment == "prod" ? 1 : 0
+resource "aws_route53_record" "runner_custom_domain_record" {
+  count = var.environment == "prod" ? 1 : 0
 
-#   allow_overwrite = true
-#   name            = "din.dev"
-#   records         = [
-#     data.terraform_remote_state.dev.outputs.proxy_deployment_dns_target[0]
-#   ]
-#   ttl             = 60
-#   type            = "CNAME"
-#   zone_id         = module.route53_zone_din_dev[0].zone_id
-# }
+  allow_overwrite = true
+  name            = "din.dev"
+  records         = [
+    data.terraform_remote_state.dev.outputs.proxy_deployment_dns_target[0]
+  ]
+  ttl             = 60
+  type            = "CNAME"
+  zone_id         = module.route53_zone_din_dev[0].zone_id
+}
+resource "aws_route53_record" "runner_custom_domain_certificate_record" {
+  count = var.environment == "prod" ? length(data.terraform_remote_state.dev.outputs.proxy_deployment_certificate_validation_records[0]) : 0
+
+  allow_overwrite = true
+  name            = data.terraform_remote_state.dev.outputs.proxy_deployment_certificate_validation_records[0].*.name[count.index]
+  records         = [
+    data.terraform_remote_state.dev.outputs.proxy_deployment_certificate_validation_records[0].*.value[count.index]
+  ]
+  ttl             = 60
+  type            = "CNAME"
+  zone_id         = module.route53_zone_din_dev[0].zone_id
+}
